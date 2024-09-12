10b95baedeeaa254d8834bc69d4f22f6a54c22c5
OthmanGh <othmanghandour51@gmail.com>
2024-09-13 00:12:54 +0300

    Add custom user model to config file

--- backend/todo_project/todo_project/settings.py
+++ backend/todo_project/todo_project/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'userauth.User'
+
+
 
 # Application definition
 
@@ -124,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
