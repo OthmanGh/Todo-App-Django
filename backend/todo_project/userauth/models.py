7fc555c89283bc40d5dd84a40d4321fd1a506291
OthmanGh <othmanghandour51@gmail.com>
2024-09-13 00:10:48 +0300

    Create custom user model

--- backend/todo_project/userauth/models.py
+++ backend/todo_project/userauth/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, role='user'):
+        if not email:
+            raise ValueError('Users must have an email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, role=role)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+
+
+    def create_superuser(self, email, username, password=None):
+        user= self.create_user(email, username, password, role='admin')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+        return user
+    
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=[('user', 'User'), ('Admin', 'admin')])
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
